f7a1c365acd1592a8da47c7188ce108dc8749419
James Vanneman <jvannem@gmail.com>
2017-10-01 12:46:02 -0700

    Minimizing content loss with early vgg convolutional output

--- utils.py
+++ utils.py
@@ -3,11 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
+import pdb
 
 mean   = [0.485, 0.456, 0.406]
 stddev = [0.229, 0.224, 0.225]
+def top(img):
+    return img[:,:-91, :]
+
 folder = ImageFolder("data/", transforms.Compose([
+    transforms.Scale(224),
     transforms.ToTensor(),
+    transforms.Lambda(top),
     transforms.Normalize(mean=mean, std=stddev),
 ]))
 
@@ -21,3 +27,5 @@ def imshow(img):
     img = unnormalize(img)
     img = img.numpy()
     plt.imshow(np.transpose(img, (1,2,0)))
+
+
